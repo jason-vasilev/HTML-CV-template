5b2e257dd231038cbdd6b78b6def4c952e4d15c0
jason-vasilev <52400967+jason-vasilev@users.noreply.github.com>
2023-10-10 23:01:10 +0200

    Add scss and minification for production mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+      chunkFilename: "[id].css",
+    }),
+  ],
   entry: './src/index.js',
   mode: 'development',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      `...`,
+    ],
   },
 };
